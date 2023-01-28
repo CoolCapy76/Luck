6c05ac32526c73054f05e4c160386dc784393d4c
CoolCapy76 <120516620+CoolCapy76@users.noreply.github.com>
2023-01-28 22:36:18 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Do you want to roll the dice y/n");
+        string choice = Console.ReadLine();
+
+        while (true)
+        {
+            if (choice == "y")
+            {
+                int y = new Random().Next(1, 7);
+
+                Console.WriteLine("Number rolled - " + y + " Do you want to play again y/n");
+
+                string choice2 = Console.ReadLine();
+
+                    if (choice2 == "y")
+                    {
+
+                    }
+
+                    if (choice2 == "n")
+                    {
+                    Environment.Exit(0);
+                    }
+            }
+
+            if (choice == "n")
+            {
+                Environment.Exit(0);
+
+;
+            }
+            
+        }
+    }
+}
